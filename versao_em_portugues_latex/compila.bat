956bf1673be16b3c680b10942d8456e502825b6b
Cristiano <bitbucket.20.culture@antichef.net>
2019-10-03 12:00:27 -0300

    Initial version.

---
+++ versao_em_portugues_latex/compila.bat
@@ -0,0 +1,13 @@
+pdflatex ppgeb_mestrado
+bibtex ppgeb_mestrado
+pdflatex ppgeb_mestrado
+pdflatex ppgeb_mestrado
+
+del "ppgeb_mestrado.aux"
+del "ppgeb_mestrado.bbl"
+del "ppgeb_mestrado.blg"
+del "ppgeb_mestrado.lof"
+del "ppgeb_mestrado.log"
+del "ppgeb_mestrado.lot"
+del "ppgeb_mestrado.out"
+del "ppgeb_mestrado.toc"
---
+++ versao_em_portugues_latex/compilation_files/my_hotkeys.vim
@@ -0,0 +1,3 @@
+map <f3> :wall<cr>:!compilation_files/compile main<cr>
+map <f7> :wall<cr>:!compilation_files/compile bib<cr>
+Portug
